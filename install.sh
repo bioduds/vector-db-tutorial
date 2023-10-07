d5b873491d1358a9404d139b88aebef70c4468b8
bioduds <bioduds@gmail.com>
2023-10-07 03:47:26 +0000

    Ready to use

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ pip install python-dotenv
 pip install faiss-cpu 
 pip install openai 
 pip install huggingface_hub
+pip install tiktoken
+pip install InstructorEmbedding
+pip install sentence_transformers
